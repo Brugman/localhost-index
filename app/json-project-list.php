7672a1274ed6afafd7bae3091dfe551f424abb43
Tim Brugman <tim@timbrugman.com>
2020-07-30 17:28:44 +0200

    Cleaned up.

--- app/json-project-list.php
+++ app/json-project-list.php
@@ -6,16 +6,15 @@ include 'functions.php';
 $projects = get_projects_basic();
 
 $projects = array_filter( $projects, function ( $item ) {
+
     if ( strpos( $item, 'New folder' ) === 0 )
         return false;
 
     return true;
 });
 
-// d( $projects );
-
 header('HTTP/1.1 200 OK');
-header( 'Content-Type: application/json' );
+header('Content-Type: application/json');
 echo json_encode( array_values( $projects ) );
 exit;
 
