d7b778cc84e97f065fd31a6088d87e4c1c06e5a7
Tim Brugman <tim@timbrugman.com>
2021-05-21 00:12:10 +0200

    Updated favicon.

--- app/core-head.php
+++ app/core-head.php
@@ -14,6 +14,11 @@
     <!-- link css -->
     <link rel="stylesheet" href="/assets/css/app.min.css">
 
+    <!-- link icon -->
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2">
+    <link rel="shortcut icon" href="/favicon.ico?v=2">
+
 </head>
 <body>
 
